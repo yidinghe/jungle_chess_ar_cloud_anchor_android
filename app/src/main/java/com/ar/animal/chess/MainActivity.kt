123777f96915f225ecba88fa55a7d4593faa1f9c
Xipeng Wang <wangxipeng0809@gmail.com>
2018-05-29 14:36:41 -0400

    Fixed cast exception

--- app/src/main/java/com/ar/animal/chess/MainActivity.kt
+++ app/src/main/java/com/ar/animal/chess/MainActivity.kt
@@ -531,7 +531,7 @@ class MainActivity : AppCompatActivity() {
             // controllerNode.renderable = controllerRenderable
 
         val ll_start_game = controllerRenderableView.findViewById<LinearLayout>(R.id.ll_start_game)
-        val btn_start_game = controllerRenderableView.findViewById<LinearLayout>(R.id.btn_start_game)
+        val btn_start_game = controllerRenderableView.findViewById<Button>(R.id.btn_start_game)
         btn_start_game.setOnClickListener{
             gameController.confirmGameStart{ _, _ ->
                 ll_start_game.visibility = GONE
